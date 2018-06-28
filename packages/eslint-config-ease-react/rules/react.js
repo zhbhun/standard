39fbcb24383e82bdd9f5770452e56595a958612f
zhbhun <zhbhun@gmail.com>
2018-06-28 17:01:42 +0800

    feat: 调整 react 部分检查规则

--- packages/eslint-config-ease-react/rules/react.js
+++ packages/eslint-config-ease-react/rules/react.js
@@ -373,7 +373,7 @@ module.exports = {
 
     // One JSX Element Per Line
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/jsx-one-expression-per-line.md
-    'react/jsx-one-expression-per-line': 'error',
+    'react/jsx-one-expression-per-line': 'off',
 
     // Enforce consistent usage of destructuring assignment of props, state, and context
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/destructuring-assignment.md
@@ -381,15 +381,11 @@ module.exports = {
 
     // Prevent using this.state within a this.setState
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/no-access-state-in-setstate.md
-    'react/no-access-state-in-setstate': 'error',
+    'react/no-access-state-in-setstate': 'off',
 
     // Prevent usage of button elements without an explicit type attribute
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/button-has-type.md
-    'react/button-has-type': ['error', {
-      button: true,
-      submit: true,
-      reset: false,
-    }],
+    'react/button-has-type': 'off',
 
     // Ensures inline tags are not rendered without spaces between them
     'react/jsx-child-element-spacing': 'off',
