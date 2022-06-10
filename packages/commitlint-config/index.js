d94f0fb1aff4eddaaf8deee2721244727f357ee2
zhbhun <zhbhun@gmail.com>
2022-06-10 17:51:24 +0800

    fix: disable commitlint subject-case

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -2,5 +2,10 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always', 1000],
+    'subject-case': [
+      0,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
   },
 }
